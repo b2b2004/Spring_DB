9feb8d91e7d7df7dc35ed7411d7194302b3acc23
Kwon yongho <b2b2004@nate.com>
2023-05-17 17:04:03 +0900

    6. Spring DB Spring Exception, JdbcTemplate

---
+++ jdbc/src/test/java/com/example/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -0,0 +1,54 @@
+package com.example.jdbc.exception.translator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static com.example.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class SpringExceptionTranslatorTest {
+
+    DataSource dataSource;
+
+    @BeforeEach
+    void init() {
+        dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+
+    @Test
+    void exceptionTranslator() {
+
+        String sql = "select bad grammar"; // sql문이 이상할 때
+
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeQuery();
+        } catch (SQLException e) {
+
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+            //org.springframework.jdbc.support.sql-error-codes.xml
+            SQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+            //org.springframework.jdbc.BadSqlGrammarException
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx", resultEx);
+
+            assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
+
+        }
+    }
+
+}
+
